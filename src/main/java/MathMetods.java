00e5d9e7cee178798e84c5ba0a8a74cd09a20c40
Soldatov Denis <soldatov_denys@mail.ru>
2017-05-16 21:11:50 +0300

    Lesson 1-2

---
+++ src/main/java/MathMetods.java
@@ -0,0 +1,9 @@
+/**
+ * Created by QA on 13.05.2017.
+ */
+public class MathMetods {
+    public static int CalculateSum(int a, int b){
+        int sum=a+b;
+        return sum;
+    }
+}
---
+++ src/test/java/MathMetodsTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by QA on 13.05.2017.
+ */
+public class MathMetodsTest {
+
+    public static void main(String[] args){
+        int x = MathMetods.CalculateSum(2, 3);
+        System.out.print(x);
+    }
+}
